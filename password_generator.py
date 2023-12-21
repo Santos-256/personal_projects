7deed03fed5013253170d0077fe6e96db4e23e70
Santos-256 <santosedward1998@gmail.com>
2023-12-21 14:33:59 +0000

    password

---
+++ password_generator.py
@@ -0,0 +1,13 @@
+lowercase = []
+for letters in range(ord('a'), ord('z') + 1):
+    lowercase += chr(letters)
+print(lowercase)
+uppercase = []
+for letters in range(ord('A'), ord('Z') + 1):
+    uppercase += chr(letters)
+print(uppercase)
+
+symbols_string = ['!', '#', '$', '%', '&', '*', '+', '-', '/', '@', '^', '<', '>', '|', '?']
+print(symbols_string)
+number = [0,1,2,3,4,5,6,7,8,9]
+print(number)
