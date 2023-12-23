14099173c8fabc366eefce78a0cb5e15922e7fb4
Santos-256 <santosedward1998@gmail.com>
2023-12-23 13:45:57 +0000

    update

--- password_generator.py
+++ password_generator.py
@@ -1,13 +1,43 @@
+import random
 lowercase = []
 for letters in range(ord('a'), ord('z') + 1):
     lowercase += chr(letters)
-print(lowercase)
+
 uppercase = []
 for letters in range(ord('A'), ord('Z') + 1):
     uppercase += chr(letters)
-print(uppercase)
+letters_upper_lower = lowercase + uppercase
+print("WELCOME TO PASSWORD GENERATOR\n")
 
-symbols_string = ['!', '#', '$', '%', '&', '*', '+', '-', '/', '@', '^', '<', '>', '|', '?']
-print(symbols_string)
-number = [0,1,2,3,4,5,6,7,8,9]
-print(number)
+symbols_string = ['!', '#', '$', '%', '&', '*', '/', '@', '^', '<', '>', '|', '?', '(', ')', '[', ']', '~']
+
+numbers_string = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+letters = input("Enter how many letters you want in your password: ")
+letters = int(letters)
+symbols = input("Enter how many symbols you want in your password: ")
+symbols = int(symbols)
+numbers = input("Enter how many numbers you want in your password: ")
+numbers = int(numbers)
+print("\n")
+
+password1 = ""
+for i in range(0,letters):
+    char = random.choice(letters_upper_lower)
+    password1 += char
+#print(password1)
+
+password2 = ""
+for i in range(0,symbols):
+    symbol = random.choice(symbols_string)
+    password2 += symbol
+#print(password2)
+
+password3 = ""
+for i in range(0,numbers):
+    num = random.choice(numbers_string)
+    password3 +=  num
+#print(password3)
+
+concat_password = password1 + password2 + password3
+print(concat_password)
